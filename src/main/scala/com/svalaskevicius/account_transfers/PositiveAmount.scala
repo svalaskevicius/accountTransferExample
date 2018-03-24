e8e1c6172b7d4a36577608d50c82b8198b41a5fa
Sarunas Valaskevicius <rakatan@gmail.com>
2018-03-24 13:57:54 +0000

    fix test: positive amount with non positive values

--- src/main/scala/com/svalaskevicius/account_transfers/PositiveAmount.scala
+++ src/main/scala/com/svalaskevicius/account_transfers/PositiveAmount.scala
@@ -1,6 +1,7 @@
 package com.svalaskevicius.account_transfers
 
 object PositiveAmount {
-  def apply(value: Long) = Some(new PositiveAmount(value))
+  def apply(value: Long) = if (value > 0) Some(new PositiveAmount(value)) else None
 }
+
 final case class PositiveAmount private (value: Long)
