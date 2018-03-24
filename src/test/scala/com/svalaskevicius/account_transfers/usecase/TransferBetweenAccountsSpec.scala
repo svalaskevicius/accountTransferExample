3a67ef3a8d4a8c256e5e42d6c5c7c80a405bf3fb
Sarunas Valaskevicius <rakatan@gmail.com>
2018-03-24 19:22:02 +0000

    convert to property based test

--- src/test/scala/com/svalaskevicius/account_transfers/usecase/TransferBetweenAccountsSpec.scala
+++ src/test/scala/com/svalaskevicius/account_transfers/usecase/TransferBetweenAccountsSpec.scala
@@ -7,21 +7,23 @@ import com.svalaskevicius.account_transfers.model.DebitError.InsufficientFunds
 import com.svalaskevicius.account_transfers.model.{Account, PositiveNumber}
 import com.svalaskevicius.account_transfers.service.{AccountService, InMemoryEventStorage}
 import com.svalaskevicius.account_transfers.usecase.TransferBetweenAccountsError.{CreditFailed, DebitFailed}
+import org.scalacheck.Gen
 import org.scalatest._
+import org.scalatest.prop.PropertyChecks
 
-class TransferBetweenAccountsSpec extends FlatSpec with Matchers {
-  "TransferBetweenAccounts" should "transfer requested amount" in {
+class TransferBetweenAccountsSpec extends FlatSpec with Matchers with PropertyChecks {
+  "TransferBetweenAccounts" should "transfer requested amount" in forAll (Gen.choose(1, 10000)) { amount =>
     val storage = new InMemoryEventStorage(Account.loader)
     val accountService = new AccountService(storage)
     val transferBetweenAccounts = new TransferBetweenAccounts(accountService)
 
     accountService.register("account_1", 10000)
-    accountService.register("account_2", 10000)
+    accountService.register("account_2", 20000)
 
-    transferBetweenAccounts("account_1", "account_2", PositiveNumber(500).get) should be(Right(()))
+    transferBetweenAccounts("account_1", "account_2", PositiveNumber(amount).get) should be(Right(()))
 
-    accountService.currentBalance("account_1") should be(Right(10000 - 500))
-    accountService.currentBalance("account_2") should be(Right(10000 + 500))
+    accountService.currentBalance("account_1") should be(Right(10000 - amount))
+    accountService.currentBalance("account_2") should be(Right(20000 + amount))
   }
 
   it should "fail when debit fails" in {
@@ -51,4 +53,6 @@ class TransferBetweenAccountsSpec extends FlatSpec with Matchers {
 
     accountService.currentBalance("account_1") should be(Right(10000))
   }
+
+
 }
