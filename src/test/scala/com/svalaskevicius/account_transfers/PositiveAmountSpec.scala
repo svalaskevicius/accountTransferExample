18280d366ca138c8ffe76b90ef9463ca9ee8699d
Sarunas Valaskevicius <rakatan@gmail.com>
2018-03-24 13:57:20 +0000

    fail test: positive amount with non positive values

--- src/test/scala/com/svalaskevicius/account_transfers/PositiveAmountSpec.scala
+++ src/test/scala/com/svalaskevicius/account_transfers/PositiveAmountSpec.scala
@@ -3,7 +3,12 @@ package com.svalaskevicius.account_transfers
 import org.scalatest._
 
 class PositiveAmountSpec extends FlatSpec with Matchers {
-  "Positive amount" should "be allowed to construct with a positive value" in {
+  "Positive amount" should "be allowed to be constructed with a positive value" in {
     PositiveAmount(1).map(_.value) should be (Some(1))
+  }
+
+  it should "not allow to be constructed with non positive value" in {
+    PositiveAmount(0).map(_.value) should be (None)
+    PositiveAmount(-1).map(_.value) should be (None)
   }
 }
