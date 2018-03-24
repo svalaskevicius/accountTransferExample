ec0891ab8031264a1bb9bdc7b1dcb0bee0ac2053
Sarunas Valaskevicius <rakatan@gmail.com>
2018-03-24 18:54:19 +0000

    fail transfer process on debit failure

--- src/main/scala/com/svalaskevicius/account_transfers/usecase/TransferBetweenAccounts.scala
+++ src/main/scala/com/svalaskevicius/account_transfers/usecase/TransferBetweenAccounts.scala
@@ -6,8 +6,14 @@ import cats.Monad
 import cats.syntax.all._
 import com.svalaskevicius.account_transfers.model.Account.AccountId
 import com.svalaskevicius.account_transfers.model.AccountEvent.TransferStarted
-import com.svalaskevicius.account_transfers.model.{AccountEvent, PositiveNumber}
+import com.svalaskevicius.account_transfers.model.{AccountEvent, DebitError, PositiveNumber}
 import com.svalaskevicius.account_transfers.service.AccountService
+import com.svalaskevicius.account_transfers.usecase.TransferBetweenAccountsError.DebitFailed
+
+sealed trait TransferBetweenAccountsError
+object TransferBetweenAccountsError {
+  case class DebitFailed(debitError: DebitError) extends TransferBetweenAccountsError
+}
 
 /**
   * While in a distributed system a process would be listening for events and emitting commands instead of
@@ -17,7 +23,7 @@ import com.svalaskevicius.account_transfers.service.AccountService
   * @param accountService
   * @tparam F
   */
-class TransferBetweenAccounts[F[_]: Monad] (accountService: AccountService[F]) {
+class TransferBetweenAccounts[F[_]] (accountService: AccountService[F])(implicit fMonad: Monad[F]) {
 
   /**
     * Debit the from account, then credit the to account and complete transaction.
@@ -28,28 +34,34 @@ class TransferBetweenAccounts[F[_]: Monad] (accountService: AccountService[F]) {
     * @param amount
     * @return
     */
-  def apply(accountFrom: AccountId, accountTo: AccountId, amount: PositiveNumber) = {
+  def apply(accountFrom: AccountId, accountTo: AccountId, amount: PositiveNumber): F[TransferBetweenAccountsError Either Unit] = {
 
-    def completeTransaction(transactionId: UUID) = accountService.completeTransfer(accountFrom, transactionId).map {
-      case Left(err) => ???
-      case Right(_) => Right(())
-    }
+    def completeTransaction(transactionId: UUID): F[TransferBetweenAccountsError Either Unit] =
+      accountService.completeTransfer(accountFrom, transactionId).map {
+        case Left(err) => ???
+        case Right(_) => Right(())
+      }
 
-    def creditForTransfer(transactionId: UUID) = accountService.creditForTransfer(accountTo, transactionId, amount).flatMap {
-      case Left(err) => ???
-      case Right(_) => completeTransaction(transactionId)
-    }
+    def creditForTransfer(transactionId: UUID): F[TransferBetweenAccountsError Either Unit] =
+      accountService.creditForTransfer(accountTo, transactionId, amount).flatMap {
+        case Left(err) => ???
+        case Right(_) => completeTransaction(transactionId)
+      }
 
-    def accountDebited(events: List[AccountEvent]) = findTransactionId(events) match {
-      case None => ???
-      case Some(transactionId) => creditForTransfer(transactionId)
-    }
+    def accountDebited(events: List[AccountEvent]): F[TransferBetweenAccountsError Either Unit] =
+      findTransactionId(events) match {
+        case None => ???
+        case Some(transactionId) => creditForTransfer(transactionId)
+      }
 
     accountService.debitForTransfer(accountFrom, accountTo, amount).flatMap {
-      case Left(err) => ???
+      case Left(err) => processFailed(DebitFailed(err))
       case Right(events) => accountDebited(events)
     }
   }
+
+  private def processFailed(err: TransferBetweenAccountsError): F[TransferBetweenAccountsError Either Unit] =
+    fMonad.pure(Left(err))
 
   private def findTransactionId(events: List[AccountEvent]) = events.collectFirst {
     case TransferStarted(id, _, _) => id
--- src/test/scala/com/svalaskevicius/account_transfers/usecase/TransferBetweenAccountsSpec.scala
+++ src/test/scala/com/svalaskevicius/account_transfers/usecase/TransferBetweenAccountsSpec.scala
@@ -1,7 +1,9 @@
 package com.svalaskevicius.account_transfers.usecase
 
+import com.svalaskevicius.account_transfers.model.DebitError.InsufficientFunds
 import com.svalaskevicius.account_transfers.model.{Account, AccountEvent, PositiveNumber}
 import com.svalaskevicius.account_transfers.service.{AccountService, InMemoryEventStorage}
+import com.svalaskevicius.account_transfers.usecase.TransferBetweenAccountsError.DebitFailed
 import org.scalatest._
 
 class TransferBetweenAccountsSpec extends FlatSpec with Matchers {
@@ -13,9 +15,23 @@ class TransferBetweenAccountsSpec extends FlatSpec with Matchers {
     accountService.register("account_1", 10000)
     accountService.register("account_2", 10000)
 
-    transferBetweenAccounts("account_1", "account_2", PositiveNumber(500).get)
+    transferBetweenAccounts("account_1", "account_2", PositiveNumber(500).get) should be (Right(()))
 
     accountService.currentBalance("account_1") should be(Right(10000 - 500))
     accountService.currentBalance("account_2") should be(Right(10000 + 500))
+  }
+
+  it should "fail when debit fails" in {
+    val storage = new InMemoryEventStorage(Account.loader)
+    val accountService = new AccountService(storage)
+    val transferBetweenAccounts = new TransferBetweenAccounts(accountService)
+
+    accountService.register("account_1", 10000)
+    accountService.register("account_2", 10000)
+
+    transferBetweenAccounts("account_1", "account_2", PositiveNumber(5000000).get) should be (Left(DebitFailed(InsufficientFunds)))
+
+    accountService.currentBalance("account_1") should be(Right(10000))
+    accountService.currentBalance("account_2") should be(Right(10000))
   }
 }
